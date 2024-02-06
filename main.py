ca4bbafa84966a7d1bd6ae91f04c0c09c3e20114
George <g.shchet@gmail.com>
2024-02-06 20:31:23 +0300

    circle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import random
 import sys
 
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QPen, QBrush
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
@@ -8,7 +11,24 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
+        self.x = self.width()
+        self.y = self.height()
+        self.flag = False
+        self.pushButton.clicked.connect(self.draw)
 
+    def paintEvent(self, event):
+        if self.flag:
+            x0, y0 = random.randint(0, self.x), random.randint(0, self.y)
+            r = random.randint(0, max(self.x, self.y))
+            painter = QPainter(self)
+            painter.setPen(QPen(Qt.yellow, 5, Qt.SolidLine))
+            painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+            painter.drawEllipse(x0, y0, r, r)
+            self.flag = False
+
+    def draw(self):
+        self.flag = True
+        self.repaint()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
